b2f7102551fc47bd78795746e79db15f363e61ee
Chooubeyji <ashutoshchoubey2004@gmail.com>
2025-06-24 20:42:04 +0530

    change in frontend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 ;
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", // replace with your backend URL
+  baseURL: "https://talksy-2-backend.vercel.app/api", // replace with your backend URL
   withCredentials: true, // send cookies with the request
 });
